c41844de02fd4e7daa845db0e56a3d84c6e8c128
sathaphorn.stp <sathaphorn.stp@gmail.com>
2019-11-24 20:33:13 +0700

    camera switch

--- App.js
+++ App.js
@@ -1,14 +1,85 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Camera } from 'expo';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as Permissions from 'expo-permissions';
+import { FontAwesome, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Camera></Camera>
-    </View>
-  );
+
+export default class App extends React.Component {
+
+  state = {
+    hasPermission: null,
+    cameraType: Camera.Constants.Type.back,
+  };
+
+  async componentDidMount() {
+    const {status} = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({hasPermission: status === 'granted'});
+  }
+
+  handleCameraType = () => {
+    const {cameraType} = this.state;
+
+    this.setState({
+      cameraType:
+        cameraType === Camera.Constants.Type.back
+          ? Camera.Constants.Type.front
+          : Camera.Constants.Type.back
+    })
+  };
+
+  render() {
+    const {hasPermission} = this.state;
+    if (hasPermission === null) {
+      return <View/>;
+    } else if (hasPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{flex: 1}}>
+          <Camera style={{flex: 1}} type={this.state.cameraType}>
+            <View style={{flex: 1, flexDirection: "row", justifyContent: "space-between", margin: 20}}>
+              <TouchableOpacity
+                style={{
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                  backgroundColor: 'transparent',
+                }}>
+                <Ionicons
+                  name="ios-photos"
+                  style={{color: "#fff", fontSize: 40}}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                  backgroundColor: 'transparent',
+                }}>
+                <FontAwesome
+                  name="camera"
+                  style={{color: "#fff", fontSize: 40}}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={()=>this.handleCameraType()}
+                style={{
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                  backgroundColor: 'transparent',
+                }}>
+                <MaterialCommunityIcons
+                  name="camera-switch"
+                  style={{color: "#fff", fontSize: 40}}
+                />
+              </TouchableOpacity>
+            </View>
+          </Camera>
+        </View>
+      );
+    }
+  }
+
 }
 
 const styles = StyleSheet.create({
---
+++ README.md
@@ -0,0 +1,12 @@
+# Expo camera demo
+
+## Icon
+- https://expo.github.io/vector-icons/
+
+
+
+## References
+- [Camera Docs Expo](https://docs.expo.io/versions/latest/sdk/camera)
+- [React Native Camera Expo Example](https://reactnativemaster.com/react-native-camera-expo-example)
+- [Capture image using React native Expo app](https://reactnative.sataiva.com/reactnative-intro/reactnative-expo-camera)
+- [Using the Expo Camera - React Native Sketch Elements](https://www.youtube.com/watch?v=S-HVfH7BVIQ)
